c3d5f555ae334423c98abcb678a76005094f8c56
Zunon <zunon@zunon.io>
2022-03-12 16:04:46 +0400

    Solve problem 1

---
+++ rust/problem_01/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut sum: u32 = 0;
+    for number in 1..1000 {
+        if (number % 3) == 0 || (number % 5) == 0 {
+            sum += number;
+        }
+    }
+    println!("{}", sum);
+}

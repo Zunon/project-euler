8d4bee92f7337b811976ffeaa30e7b92f51395c7
Zunon <zunon@zunon.io>
2022-03-12 16:28:51 +0400

    Optimize the solution for problem 1
    By using the definition of the union of two sets:
     A∪B = A + B - A∩B
    Where A is the sum of all numbers below 1000 divisible by 3
    B is the sum of all numbers below 1000 divisible by 5
    A∩B is the sum of all numbers below 1000 divisible by 15
    The solution is way more efficient compared to just looping over
    all numbers below 1000 and checking if they're divisible by 5 and
    3.

--- rust/problem_01/src/main.rs
+++ rust/problem_01/src/main.rs
@@ -1,9 +1,11 @@
 fn main() {
+    println!("{}", (sum_divisible_by(3, 1000) + sum_divisible_by(5, 1000) - sum_divisible_by(15, 1000)));
+}
+
+fn sum_divisible_by(n: u32, max: u32) -> u32 {
     let mut sum: u32 = 0;
-    for number in 1..1000 {
-        if (number % 3) == 0 || (number % 5) == 0 {
-            sum += number;
-        }
+    for number in 1..(max / n) {
+        sum += number * n;
     }
-    println!("{}", sum);
+    sum
 }
